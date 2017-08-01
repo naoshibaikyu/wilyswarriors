ba0a33d5e69218016f931595aaf2593ad44a6f87
naoshibaikyu <msonic32x@hotmail.com>
2017-07-31 21:39:54 -0400

    hey

--- assets/js/mugshots.js
+++ assets/js/mugshots.js
@@ -5,77 +5,77 @@ var directory = "./assets/images/mugshots/";
 //Cyborg Resistance Members
 
 //Concrete Man
-	document.body.innerHTML = document.body.innerHTML.replace(/Concrete:/gi, '<img src=' + directory + 'Concrete1.png> <b>Concrete Man:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Concrete [Happy]:/gi, '<img src=' + directory + 'Concrete2.png> <b>Concrete Man:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Concrete [Glare]:/gi, '<img src=' + directory + 'Concrete3.png> <b>Concrete Man:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Concrete [Angry]:/gi, '<img src=' + directory + 'Concrete4.png> <b>Concrete Man:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Concrete [Shocked]:/gi, '<img src=' + directory + 'Concrete5.png> <b>Concrete Man:</b>'); //Shocked
-	document.body.innerHTML = document.body.innerHTML.replace(/Concrete [Aloof]:/gi, '<img src=' + directory + 'Concrete6.png> <b>Concrete Man:</b>'); //Aloof
+	document.body.innerHTML.replace(/Concrete:/gi, '<img src=' + directory + 'Concrete1.png> <b>Concrete Man:</b>'); //Normal
+	document.body.innerHTML.replace(/Concrete [Happy]:/gi, '<img src=' + directory + 'Concrete2.png> <b>Concrete Man:</b>'); //Happy
+	document.body.innerHTML.replace(/Concrete [Glare]:/gi, '<img src=' + directory + 'Concrete3.png> <b>Concrete Man:</b>'); //Glare
+	document.body.innerHTML.replace(/Concrete [Angry]:/gi, '<img src=' + directory + 'Concrete4.png> <b>Concrete Man:</b>'); //Angry
+	document.body.innerHTML.replace(/Concrete [Shocked]:/gi, '<img src=' + directory + 'Concrete5.png> <b>Concrete Man:</b>'); //Shocked
+	document.body.innerHTML.replace(/Concrete [Aloof]:/gi, '<img src=' + directory + 'Concrete6.png> <b>Concrete Man:</b>'); //Aloof
 	
 //Magma Man
-	document.body.innerHTML = document.body.innerHTML.replace(/Magma:/gi, '<img src=' + directory + 'Magma1.png> <b>Magma Man:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Magma [Happy]:/gi, '<img src=' + directory + 'Magma2.png> <b>Magma Man:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Magma [Glare]:/gi, '<img src=' + directory + 'Magma3.png> <b>Magma Man:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Magma [Angry]:/gi, '<img src=' + directory + 'Magma4.png> <b>Magma Man:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Magma [Shocked]:/gi, '<img src=' + directory + 'Magma5.png> <b>Magma Man:</b>'); //Shocked
+	document.body.innerHTML.replace(/Magma:/gi, '<img src=' + directory + 'Magma1.png> <b>Magma Man:</b>'); //Normal
+	document.body.innerHTML.replace(/Magma [Happy]:/gi, '<img src=' + directory + 'Magma2.png> <b>Magma Man:</b>'); //Happy
+	document.body.innerHTML.replace(/Magma [Glare]:/gi, '<img src=' + directory + 'Magma3.png> <b>Magma Man:</b>'); //Glare
+	document.body.innerHTML.replace(/Magma [Angry]:/gi, '<img src=' + directory + 'Magma4.png> <b>Magma Man:</b>'); //Angry
+	document.body.innerHTML.replace(/Magma [Shocked]:/gi, '<img src=' + directory + 'Magma5.png> <b>Magma Man:</b>'); //Shocked
 
 //Hornet Man
 	document.body.innerHTML.replace(/Hornet:/gi, '<img src=' + directory + 'Hornet1.png> <b>Hornet Man:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Hornet [Happy]:/gi, '<img src=' + directory + 'Hornet2.png> <b>Hornet Man:</b>'); //Rolling Eyes. Note how it's impossible for him to be happy so he breaks the pattern
-	document.body.innerHTML = document.body.innerHTML.replace(/Hornet [Glare]:/gi, '<img src=' + directory + 'Hornet3.png> <b>Hornet Man:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Hornet [Angry]:/gi, '<img src=' + directory + 'Hornet4.png> <b>Hornet Man:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Hornet [Shocked]:/gi, '<img src=' + directory + 'Hornet5.png> <b>Hornet Man:</b>'); //Shocked
-	document.body.innerHTML = document.body.innerHTML.replace(/Hornet [Pissed]:/gi, '<img src=' + directory + 'Hornet6.png> <b>Hornet Man:</b>'); //Pissed
+	document.body.innerHTML.replace(/Hornet [Happy]:/gi, '<img src=' + directory + 'Hornet2.png> <b>Hornet Man:</b>'); //Rolling Eyes. Note how it's impossible for him to be happy so he breaks the pattern
+	document.body.innerHTML.replace(/Hornet [Glare]:/gi, '<img src=' + directory + 'Hornet3.png> <b>Hornet Man:</b>'); //Glare
+	document.body.innerHTML.replace(/Hornet [Angry]:/gi, '<img src=' + directory + 'Hornet4.png> <b>Hornet Man:</b>'); //Angry
+	document.body.innerHTML.replace(/Hornet [Shocked]:/gi, '<img src=' + directory + 'Hornet5.png> <b>Hornet Man:</b>'); //Shocked
+	document.body.innerHTML.replace(/Hornet [Pissed]:/gi, '<img src=' + directory + 'Hornet6.png> <b>Hornet Man:</b>'); //Pissed
 	
 //Galaxy Man
-	document.body.innerHTML = document.body.innerHTML.replace(/Galaxy:/gi, '<img src=' + directory + 'Galaxy1.png> <b>Galaxy Man:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Galaxy [Happy]:/gi, '<img src=' + directory + 'Galaxy2.png> <b>Galaxy Man:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Galaxy [Glare]:/gi, '<img src=' + directory + 'Galaxy3.png> <b>Galaxy Man:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Galaxy [Angry]:/gi, '<img src=' + directory + 'Galaxy4.png> <b>Galaxy Man:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Galaxy [Shocked]:/gi, '<img src=' + directory + 'Galaxy5.png> <b>Galaxy Man:</b>'); //Shocked
-	document.body.innerHTML = document.body.innerHTML.replace(/Galaxy [Pissed]:/gi, '<img src=' + directory + 'Galaxy6.png> <b>Galaxy Man:</b>'); //Pissed
+	document.body.innerHTML.replace(/Galaxy:/gi, '<img src=' + directory + 'Galaxy1.png> <b>Galaxy Man:</b>'); //Normal
+	document.body.innerHTML.replace(/Galaxy [Happy]:/gi, '<img src=' + directory + 'Galaxy2.png> <b>Galaxy Man:</b>'); //Happy
+	document.body.innerHTML.replace(/Galaxy [Glare]:/gi, '<img src=' + directory + 'Galaxy3.png> <b>Galaxy Man:</b>'); //Glare
+	document.body.innerHTML.replace(/Galaxy [Angry]:/gi, '<img src=' + directory + 'Galaxy4.png> <b>Galaxy Man:</b>'); //Angry
+	document.body.innerHTML.replace(/Galaxy [Shocked]:/gi, '<img src=' + directory + 'Galaxy5.png> <b>Galaxy Man:</b>'); //Shocked
+	document.body.innerHTML.replace(/Galaxy [Pissed]:/gi, '<img src=' + directory + 'Galaxy6.png> <b>Galaxy Man:</b>'); //Pissed
 	
 //Plug Man
-	document.body.innerHTML = document.body.innerHTML.replace(/Plug:/gi, '<img src=' + directory + 'Plug1.png> <b>Plug Man:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Plug [Happy]:/gi, '<img src=' + directory + 'Plug2.png> <b>Plug Man:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Plug [Glare]:/gi, '<img src=' + directory + 'Plug3.png> <b>Plug Man:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Plug [Angry]:/gi, '<img src=' + directory + 'Plug4.png> <b>Plug Man:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Plug [Shocked]:/gi, '<img src=' + directory + 'Plug5.png> <b>Plug Man:</b>'); //Shocked
+	document.body.innerHTML.replace(/Plug:/gi, '<img src=' + directory + 'Plug1.png> <b>Plug Man:</b>'); //Normal
+	document.body.innerHTML.replace(/Plug [Happy]:/gi, '<img src=' + directory + 'Plug2.png> <b>Plug Man:</b>'); //Happy
+	document.body.innerHTML.replace(/Plug [Glare]:/gi, '<img src=' + directory + 'Plug3.png> <b>Plug Man:</b>'); //Glare
+	document.body.innerHTML.replace(/Plug [Angry]:/gi, '<img src=' + directory + 'Plug4.png> <b>Plug Man:</b>'); //Angry
+	document.body.innerHTML.replace(/Plug [Shocked]:/gi, '<img src=' + directory + 'Plug5.png> <b>Plug Man:</b>'); //Shocked
 	
 //Tornado Man
-	document.body.innerHTML = document.body.innerHTML.replace(/Tornado:/gi, '<img src=' + directory + 'Tornado1.png> <b>Tornado Man:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Tornado [Happy]:/gi, '<img src=' + directory + 'Tornado2.png> <b>Tornado Man:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Tornado [Glare]:/gi, '<img src=' + directory + 'Tornado3.png> <b>Tornado Man:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Tornado [Angry]:/gi, '<img src=' + directory + 'Tornado4.png> <b>Tornado Man:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Tornado [Shocked]:/gi, '<img src=' + directory + 'Tornado5.png> <b>Tornado Man:</b>'); //Shocked
+	document.body.innerHTML.replace(/Tornado:/gi, '<img src=' + directory + 'Tornado1.png> <b>Tornado Man:</b>'); //Normal
+	document.body.innerHTML.replace(/Tornado [Happy]:/gi, '<img src=' + directory + 'Tornado2.png> <b>Tornado Man:</b>'); //Happy
+	document.body.innerHTML.replace(/Tornado [Glare]:/gi, '<img src=' + directory + 'Tornado3.png> <b>Tornado Man:</b>'); //Glare
+	document.body.innerHTML.replace(/Tornado [Angry]:/gi, '<img src=' + directory + 'Tornado4.png> <b>Tornado Man:</b>'); //Angry
+	document.body.innerHTML.replace(/Tornado [Shocked]:/gi, '<img src=' + directory + 'Tornado5.png> <b>Tornado Man:</b>'); //Shocked
 
 //Fakette
-	document.body.innerHTML = document.body.innerHTML.replace(/Fake:/gi, '<img src=' + directory + 'Fake1.png> <b>Fakette:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Fake [Happy]:/gi, '<img src=' + directory + 'Fake2.png> <b>Fakette:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Fake [Glare]:/gi, '<img src=' + directory + 'Fake3.png> <b>Fakette:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Fake [Angry]:/gi, '<img src=' + directory + 'Fake4.png> <b>Fakette:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Fake [Shocked]:/gi, '<img src=' + directory + 'Fake5.png> <b>Fakette:</b>'); //Shocked
+	document.body.innerHTML.replace(/Fake:/gi, '<img src=' + directory + 'Fake1.png> <b>Fakette:</b>'); //Normal
+	document.body.innerHTML.replace(/Fake [Happy]:/gi, '<img src=' + directory + 'Fake2.png> <b>Fakette:</b>'); //Happy
+	document.body.innerHTML.replace(/Fake [Glare]:/gi, '<img src=' + directory + 'Fake3.png> <b>Fakette:</b>'); //Glare
+	document.body.innerHTML.replace(/Fake [Angry]:/gi, '<img src=' + directory + 'Fake4.png> <b>Fakette:</b>'); //Angry
+	document.body.innerHTML.replace(/Fake [Shocked]:/gi, '<img src=' + directory + 'Fake5.png> <b>Fakette:</b>'); //Shocked
 	
 //Jewel Woman
-	document.body.innerHTML = document.body.innerHTML.replace(/Jewel:/gi, '<img src=' + directory + 'Jewel1.png> <b>Jewel Woman:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Jewel [Happy]:/gi, '<img src=' + directory + 'Jewel2.png> <b>Jewel Woman:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Jewel [Glare]:/gi, '<img src=' + directory + 'Jewel3.png> <b>Jewel Woman:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Jewel [Angry]:/gi, '<img src=' + directory + 'Jewel4.png> <b>Jewel Woman:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Jewel [Shocked]:/gi, '<img src=' + directory + 'Jewel5.png> <b>Jewel Woman:</b>'); //Shocked
+	document.body.innerHTML.replace(/Jewel:/gi, '<img src=' + directory + 'Jewel1.png> <b>Jewel Woman:</b>'); //Normal
+	document.body.innerHTML.replace(/Jewel [Happy]:/gi, '<img src=' + directory + 'Jewel2.png> <b>Jewel Woman:</b>'); //Happy
+	document.body.innerHTML.replace(/Jewel [Glare]:/gi, '<img src=' + directory + 'Jewel3.png> <b>Jewel Woman:</b>'); //Glare
+	document.body.innerHTML.replace(/Jewel [Angry]:/gi, '<img src=' + directory + 'Jewel4.png> <b>Jewel Woman:</b>'); //Angry
+	document.body.innerHTML.replace(/Jewel [Shocked]:/gi, '<img src=' + directory + 'Jewel5.png> <b>Jewel Woman:</b>'); //Shocked
 
 
 //Splash Man
-	document.body.innerHTML = document.body.innerHTML.replace(/Splash:/gi, '<img src=' + directory + 'Splash1.png> <b>Splash Man:</b>'); //Normal
-	document.body.innerHTML = document.body.innerHTML.replace(/Splash [Happy]:/gi, '<img src=' + directory + 'Splash2.png> <b>Splash Man:</b>'); //Happy
-	document.body.innerHTML = document.body.innerHTML.replace(/Splash [Glare]:/gi, '<img src=' + directory + 'Splash3.png> <b>Splash Man:</b>'); //Glare
-	document.body.innerHTML = document.body.innerHTML.replace(/Splash [Angry]:/gi, '<img src=' + directory + 'Splash4.png> <b>Splash Man:</b>'); //Angry
-	document.body.innerHTML = document.body.innerHTML.replace(/Splash [Shocked]:/gi, '<img src=' + directory + 'Splash5.png> <b>Splash Man:</b>'); //Shocked
+	document.body.innerHTML.replace(/Splash:/gi, '<img src=' + directory + 'Splash1.png> <b>Splash Man:</b>'); //Normal
+	document.body.innerHTML.replace(/Splash [Happy]:/gi, '<img src=' + directory + 'Splash2.png> <b>Splash Man:</b>'); //Happy
+	document.body.innerHTML.replace(/Splash [Glare]:/gi, '<img src=' + directory + 'Splash3.png> <b>Splash Man:</b>'); //Glare
+	document.body.innerHTML.replace(/Splash [Angry]:/gi, '<img src=' + directory + 'Splash4.png> <b>Splash Man:</b>'); //Angry
+	document.body.innerHTML.replace(/Splash [Shocked]:/gi, '<img src=' + directory + 'Splash5.png> <b>Splash Man:</b>'); //Shocked
 	
 //Generic Humans		
 
 
 //No image
-	document.body.innerHTML = document.body.innerHTML.replace(/Narrator:/gi, '<b>Narrator:</b>');
+	document.body.innerHTML.replace(/Narrator:/gi, '<b>Narrator:</b>');
 
 //Format
 	document.body.style.backgroundColor = "#101010";
