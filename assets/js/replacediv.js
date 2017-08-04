41323c47710154ffa11a4f44873d09eac09a0d7f
jpinsent <joshpinsent@gmail.com>
2017-08-03 21:12:46 -0400

    replacing p tags with divs

--- assets/js/replacediv.js
+++ assets/js/replacediv.js
@@ -4,7 +4,7 @@ var pTags=document.getElementsByTagName('p');
 for(i=0;i<pTags.length;)
 {
     var p = pTags[i];  
-    var div = document.createElement('div');
+    var div = document.createElement('div class=content');
     div.innerHTML = p.innerHTML;
     p.parentNode.replaceChild(div, p);
 }
