98acef3f6b398ee3ed091221d14576370a64cb17
jpinsent <joshpinsent@gmail.com>
2017-08-03 21:17:18 -0400

    replacing p tags with divs

--- assets/js/replacediv.js
+++ assets/js/replacediv.js
@@ -4,7 +4,8 @@ var pTags=document.getElementsByTagName('p');
 for(i=0;i<pTags.length;)
 {
     var p = pTags[i];  
-    var div = document.createElement('div class=content');
+    var div = document.createElement('div');
+    div.className = "content";
     div.innerHTML = p.innerHTML;
     p.parentNode.replaceChild(div, p);
 }
