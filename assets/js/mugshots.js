bb23afd8cca7846a9f7d99dedf5038b6bfbe5e0c
naoshibaikyu <msonic32x@hotmail.com>
2017-10-29 18:13:31 -0400

    Signed-off-by: naoshibaikyu <msonic32x@hotmail.com>

--- assets/js/mugshots.js
+++ assets/js/mugshots.js
@@ -79,11 +79,12 @@ for (var nameKey in names)
 		//TODO: check and see if replace path exists, if not, bold name and continue, else...
 		document.body.innerHTML = document.body.innerHTML.replace(find, replace);
 	}
-	 height: 100%;
+
 }
 
 //Format
 document.body.style.backgroundColor = "#101010";
 document.body.style.color = "#C0C0C0";
 document.body.style.fontSize = "medium";
+
 
