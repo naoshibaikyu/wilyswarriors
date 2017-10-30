7897dbbb37d99375c78325f51fe8dfed687aa602
naoshibaikyu <msonic32x@hotmail.com>
2017-10-29 21:30:08 -0400

    Signed-off-by: naoshibaikyu <msonic32x@hotmail.com>

--- assets/js/mugshots.js
+++ assets/js/mugshots.js
@@ -74,7 +74,7 @@ for (var nameKey in names)
 		var find = new RegExp(str, "gi");
 		var imagePath = directory + nameKey + emoteKey + ".png"; 
 
-		replace = "<img id=double src=" + imagePath + "> <p><b>" + prefix + suffix + ":</p></p>";
+		replace = "<img id=double src=" + imagePath + "> <p><b>" + prefix + suffix + ":</b></p>";
 
 		//TODO: check and see if replace path exists, if not, bold name and continue, else...
 		document.body.innerHTML = document.body.innerHTML.replace(find, replace);
