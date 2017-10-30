ff63420bd8f994ce9657f9a95c3f93397db65c30
naoshibaikyu <msonic32x@hotmail.com>
2017-10-30 07:55:47 -0400

    Signed-off-by: naoshibaikyu <msonic32x@hotmail.com>

--- assets/js/replacediv.js
+++ assets/js/replacediv.js
@@ -1,12 +1,12 @@
 //Replace all p tags with div class=content tags
-var pTags=document.getElementsByTagName('p');
+var pTags=document.getElementsByTagName('test');
 
 for(i=0;i<pTags.length;i++)
 {
     var line = pTags[i];  
     var p = document.createElement('p');
     var div = document.createElement('div');
-    div.className = "contenthhh";
+    div.className = "content";
 
     //Surround inner HTML with p tags
     p.innerHTML = line.innerHTML;
