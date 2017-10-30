3d910f8d5711dd34210801df2471dc40b175de94
naoshibaikyu <msonic32x@hotmail.com>
2017-10-30 07:45:23 -0400

    Signed-off-by: naoshibaikyu <msonic32x@hotmail.com>

--- assets/js/mugshots.js
+++ assets/js/mugshots.js
@@ -75,7 +75,7 @@ for (var nameKey in names)
 		var find = new RegExp(str, "gi");
 		var imagePath = directory + nameKey + emoteKey + ".png"; 
 
-		replace = "<img id=double src=" + imagePath + "> <p><b>" + prefix + suffix + ":aaaaaaa</b>aaaa</br>";
+		replace = "<div class="content" "mugshot"> <img id=double src=" + imagePath + "> <p><b>" + prefix + suffix + ":</b></br></div>";
 		
 		//TODO: check and see if replace path exists, if not, bold name and continue, else...
 		document.body.innerHTML = document.body.innerHTML.replace(find, replace);
