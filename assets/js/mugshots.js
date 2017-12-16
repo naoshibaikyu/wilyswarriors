738d9129b50a3f7af4dd36a242f91055e0aaeb5a
naoshibaikyu <msonic32x@hotmail.com>
2017-12-16 16:45:14 -0500

    Signed-off-by: naoshibaikyu <msonic32x@hotmail.com>

--- assets/js/mugshots.js
+++ assets/js/mugshots.js
@@ -16,7 +16,7 @@ var names = {concrete:"Concrete",
 	jewel:"Jewel",
 	splash:"Splash",
 	narrator:"Narrator",
-	pasthornet:"PastHornet"
+	pasthornet:"Past Hornet"
 };
 
 var emotes = {original:"",
@@ -64,9 +64,6 @@ for (var nameKey in names)
 				break;
 			case "spike":
 				prefix = "Concrete";
-				suffix = " Man";
-			case "PastHornet":
-				prefix = "Splash";
 				suffix = " Man";
 				break;
 			case "narrator":
