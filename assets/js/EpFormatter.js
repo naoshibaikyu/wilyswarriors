f442306c0e4a0b8785fb27b77610008ca268f037
naoshibaikyu <30607847+naoshibaikyu@users.noreply.github.com>
2018-10-20 17:02:03 -0400

    Update assets/js/EpFormatter.js

--- assets/js/EpFormatter.js
+++ assets/js/EpFormatter.js
@@ -172,3 +172,29 @@ function insertMugshots()
 	document.body.style.color = "#C0C0C0";
 	document.body.style.fontSize = "medium";
 }
+
+
+// Showdown markdown functionality
+
+showdown.setOption("strikethrough", "true");
+showdown.setOption("tables", "true");
+
+var text = document.getElementById("act1").innerHTML,
+    target = document.getElementById("targetDiv"),
+    converter = new showdown.Converter(),
+    html = converter.makeHtml(text);
+
+    target.innerHTML = html;
+
+// Page turner function. Are we keeping this?
+
+function pageTurn(sourceDiv)
+{
+    text = document.getElementById(sourceDiv).innerHTML;
+    target = document.getElementById("targetDiv");
+    converter = new showdown.Converter();
+    html = converter.makeHtml(text);
+
+    target.innerHTML = html;
+}
+
